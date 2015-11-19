a1e8893d8624234ad37d63ad01fe68c811cf1970
manvedu <mavelandiad@gmail.com>
2015-11-19 17:33:56 -0500

    Fix form_simple_for paths

--- app/controllers/line_entries_controller.rb
+++ app/controllers/line_entries_controller.rb
@@ -8,6 +8,8 @@ class LineEntriesController < ApplicationController
 
   def new
     @line_entry = current_user.line_entries.build
+    @url = line_entries_path
+    @method =  'post'
   end
 
   def create
@@ -17,7 +19,7 @@ class LineEntriesController < ApplicationController
     if @line_entry.save
       redirect_to edit_line_entry_path(params[:line_entries], @line_entry)
     else
-      render 'edit'
+      render 'new'
     end
   end
 
@@ -25,6 +27,8 @@ class LineEntriesController < ApplicationController
     @line_entry = LineEntry.find(params[:id])
     @mentions = @line_entry.followups.map { |followup| show_mentions(followup.description) }.uniq.flatten
     @hashtags = @line_entry.followups.map { |followup| show_hashtags(followup.description) }.uniq.flatten
+    @url = "/proposals/#{params[:id]}"
+    @method = 'patch'
   end
 
   def update
@@ -40,7 +44,7 @@ class LineEntriesController < ApplicationController
     attachments = params[:line_entry][:followups_attributes]["0"][:attachments]
     followup.documents = attachments 
     followup.save!
-    redirect_to edit_line_entry_path('proposals', @line_entry)
+    redirect_to edit_line_entry_path(params[:line_entries], @line_entry)
   end
     
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/:line_entries/new/' , to: 'line_entries#new' , as: 'new_line_entries', constraints: { line_entries: /\w+/}
   post '/:line_entries/', to: 'line_entries#create', constraints: { line_entries: /\w+/}
   get '/:line_entries/:id/edit', to: 'line_entries#edit', as: 'edit_line_entry', constraints: { line_entries: /\w+/}
-  get '/noestaaki/:id', to: 'line_entries#show', as: 'line_entry'#, constraints: { line_entries: /\w+/}
+  get '/noestaaki/:id', to: 'line_entries#edit', as: 'line_entry'#, constraints: { line_entries: /\w+/}
   patch '/:line_entries/:id', to: 'line_entries#update', constraints: { line_entries: /\w+/}
 
   devise_for :users
