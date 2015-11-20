a09dd39e68216e17405c7b10cc2cb0836c243389
Julian Vargas <julianvargasalvarez@gmail.com>
2015-11-20 08:14:04 -0500

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  resources :lines
+
   get '/:line_entries/', to: 'line_entries#index', as: 'line_entries', constraints: { line_entries: /\w+/}
   get '/:line_entries/new/' , to: 'line_entries#new' , as: 'new_line_entries', constraints: { line_entries: /\w+/}
   post '/:line_entries/', to: 'line_entries#create', constraints: { line_entries: /\w+/}
   get '/:line_entries/:id/edit', to: 'line_entries#edit', as: 'edit_line_entry', constraints: { line_entries: /\w+/}
   patch '/:line_entries/:id', to: 'line_entries#update', constraints: { line_entries: /\w+/}
 
-  devise_for :users
-  resources :lines
   root to: "home#index"
 end
