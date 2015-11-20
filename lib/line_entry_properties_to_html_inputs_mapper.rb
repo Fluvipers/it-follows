068ace2cde3d9238b2f819c46fa37133bd659939
manvedu <mavelandiad@gmail.com>
2015-11-20 17:56:43 -0500

    Refactor on library

--- lib/line_entry_properties_to_html_inputs_mapper.rb
+++ lib/line_entry_properties_to_html_inputs_mapper.rb
@@ -6,25 +6,53 @@ class LineEntryPropertiesToHtmlInputsMapper
   end
 
   def map_properties
-    name_context = create_name_context
-    id_context = create_id_context
+    name_context = build_name_context
+    id_context = build_id_context
 
     @properties.map do |property|
-      [content_tag(:label, "#{property[:name].titleize}", for: "#{id_context}_#{property[:name].downcase}"),
-      content_tag(:input, "", name: "#{name_context}[#{property[:name].downcase}]", id: "#{id_context}_#{property[:name].downcase}", 
-      required: (:required if property[:required]))]
+      label_title = property_name(property)
+      input_id = build_input_id(id_context, property)
+      input_name = build_input_name(name_context, property)
+
+      [build_label(label_title, input_id), build_input(input_name, input_id, property)]
 
     end.flatten
   end
 
   private
-  def create_name_context
+
+  def property_name(property)
+    property[:name].downcase
+  end
+
+  def build_input_name(name, property)
+    "#{name}[#{property_name(property)}]"
+  end
+
+  def build_input_id(context, property)
+    "#{context}_#{property_name(property)}"
+  end
+
+  def build_label(title, input_id)
+    content_tag(:label, title.titleize, for: input_id)
+  end
+
+  def build_input(name, input_id, property)
+    content_tag(:input, nil, name: name, id: input_id,
+      required: make_it_required(property))
+  end
+
+  def make_it_required(property)
+    :required if property[:required]
+  end
+
+  def build_name_context
     context = @context.dup
     base = context.shift.to_s
     "#{base}[#{context.join("][")}]"
   end
 
-  def create_id_context
+  def build_id_context
     context = @context.dup
     "#{context.join("_")}"
   end
