cfeb46f1810ef5c5368233361880e8b8f3c55c72
manvedu <mavelandiad@gmail.com>
2015-12-16 10:46:30 -0500

    Validates fields of data

--- app/models/line_entry.rb
+++ app/models/line_entry.rb
@@ -6,6 +6,8 @@ class LineEntry < ActiveRecord::Base
   has_many :tasks, through: :followups
 
   accepts_nested_attributes_for :followups
+  validates_presence_of :data
+  validate :validation_parameters
 
   def current_percentage
     followups.last.try(:percentage).to_i
@@ -14,4 +16,12 @@ class LineEntry < ActiveRecord::Base
   def needs_followup?
     current_percentage < 100
   end
+
+  def validation_parameters
+    number_params = self.data.count
+    fields = self.data.first(number_params).map{|value| value[0] if value[1].empty?}
+    o = fields.compact.empty? ? false : true
+    errors.add(:data, "tiene más de un link") if  o
+  end
+
 end
