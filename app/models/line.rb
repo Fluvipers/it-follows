9b643bd232ecf338542c4f84f66f6b4b57aea2c8
josediaz16 <jldiaz1612@gmail.com>
2015-12-28 12:24:36 -0500

    add test to check if a line name already exist

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,7 +1,9 @@
 class Line < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :name, :properties, :user
   has_many :line_entries, dependent: :destroy
 
+  validates :name, uniqueness: { case_sensitive: false, message: "There is already a line with that name"}
   before_save :set_slug_name
 
   private
