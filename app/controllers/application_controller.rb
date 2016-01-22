0f07de3d21a817581c66f2ab68f266f6db96bd47
manvedu <mavelandiad@gmail.com>
2016-01-22 15:58:58 -0500

    add aplication controller for token authenticable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
-  acts_as_token_authentication_handler_for User 
-  before_action :authenticate_user!
+  acts_as_token_authentication_handler_for User, if: Proc.new { |c| c.request.format == 'application/json' }
+  before_action :check_user_is_authenticated!
   before_action :set_lines
   respond_to :html, :json
 
@@ -11,5 +11,14 @@ class ApplicationController < ActionController::Base
 
   def set_lines
     @lines = Line.all
+  end
+
+  def check_user_is_authenticated!
+    if params[:token]
+      users = User.where(authentication_token: params[:token])
+      users.one? ?  sign_in(:user, users.first) : authenticate_user!
+    else
+      authenticate_user!
+    end
   end
 end
