1523476dbfadc5a1bde3035daeec3888f02e10d3
manvedu <mavelandiad@gmail.com>
2016-05-12 16:02:15 -0500

    Add default property for lines

--- app/models/line.rb
+++ app/models/line.rb
@@ -6,10 +6,15 @@ class Line < ActiveRecord::Base
 
   validates :name, uniqueness: { case_sensitive: false, message: "There is already a line with that name"}
   before_save :set_slug_name
+  before_save :create_property_name
 
   accepts_nested_attributes_for :properties
 
   private
+  def create_property_name
+    self.properties.create!(name: 'name', required: true)
+  end
+
   def set_slug_name
     self.slug_name = self.name.parameterize(sep="_")
   end
